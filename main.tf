04f1dd4775dba40fc503ec65c7236146187c8ed3
paragpatil01 <parag.patil2411@gmail.com>
2023-11-01 10:35:30 +0530

    error solving

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "demo" {
   ami                     = "ami-0fc5d935ebf8bc3bc"
   instance_type           =  var.instance_type
   key_name                = "myawslogin"
-  tags                    = {
+  tags = {
     name = "testinfra"
   }
   
